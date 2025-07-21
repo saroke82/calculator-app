561067f02f77bcff3c039b56a7a28d6275dbc3e9
saroke82 <saroke82@gmail.com>
2025-07-21 17:16:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+let display = document.getElementById("display");
+
+function appendValue(value) {
+  if (display.innerText === "0" && value !== ".") {
+    display.innerText = value;
+  } else {
+    display.innerText += value;
+  }
+}
+
+function clearDisplay() {
+  display.innerText = "0";
+}
+
+function calculateResult() {
+  try {
+    display.innerText = eval(display.innerText);
+  } catch {
+    display.innerText = "Error";
+  }
+}
